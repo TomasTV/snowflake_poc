0637a801052196dd1947beb58d92277bb557fbb4
TomasTV <79015330+TomasTV@users.noreply.github.com>
2024-10-18 10:40:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,8 @@ import requests
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+#st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title("Example Streamlit App :cup_with_straw:")
